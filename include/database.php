0930bab59ed4abdea3e806fde590941df494a6d4
sixtored <sixtod@gmail.com>
2022-04-14 12:27:18 -0300

    Actulizacion web sixtored

---
+++ include/database.php
@@ -0,0 +1,8 @@
+<?php
+
+	mysql_connect("localhost","bbbmmm","xxxx") or
+						die("No se ha podido conectar a la base de datos");
+		
+	mysql_select_db("bbnnn") or 
+						die("No se ha podido seleccionar la base de datos");
+?>
